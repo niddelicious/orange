0bb44bb13b272c5f43d6f3c9ac37835b721b0e21
Nidde Nedelius <nidde@visionworx.se>
2017-08-08 21:02:11 +0200

    Created relations for Post, Tag, Category

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+
+    public function tags()
+    {
+        return $this->hasMany(Tag::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     //
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
