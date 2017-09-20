14a5132b85d61ee3ea3df6613eb2a6c7cfaee4c7
Nidde Nedelius <nidde.nedelius@elicit.se>
2017-09-20 15:36:17 +0200

    Create frontpage layout

--- resources/views/orange/home/content.blade.php
+++ resources/views/orange/home/content.blade.php
@@ -2,24 +2,60 @@
 
 @section ('content')
 
-    @include ('orange.shared.menu')
-
     <div class="container">
-        @include ('orange.home.header')
+        <section class="layout__header">
+            @include ('orange.shared.menu')
+            @include ('orange.home.header')
+            @include ('orange.shared.nav')
+        </section>
+        <section class="layout_content">
+            <div class="content">
 
-        @include ('orange.shared.nav')
-        
-        <div class="content">
+                <div class="trio">
+                    @foreach ( $trio as $post )
 
+                        @include('orange.home.partials.triobox')
 
-            <div class="trio">
-                @foreach ( $trio as $post )
+                    @endforeach
+                </div>
 
-                    @include('orange.home.partials.triobox')
+                <section class="frontpage">
+                    <section class="frontpage__list">
 
-                @endforeach
+                        <div class="frontpage__list--header">Articles</div>
+                        <div class="frontpage__list--list">
+                            @include('orange.home.partials.article')
+                            @include('orange.home.partials.article')
+                            @include('orange.home.partials.article')
+                            @include('orange.home.partials.article')
+                            @include('orange.home.partials.article')
+                            @include('orange.home.partials.article')
+                        </div>
+
+                    </section>
+                    <aside class="frontpage__aside">
+                        <ul>
+                            <li>Item 1</li>
+                            <li>Item 2</li>
+                            <li>Item 3</li>
+                            <li>Item 4</li>
+                            <li>Item 5</li>
+                            <li>Item 6</li>
+                            <li>Item 7</li>
+                            <li>Item 8</li>
+                            <li>Item 9</li>
+                            <li>Item 10</li>
+                            <li>Item 11</li>
+                            <li>Item 12</li>
+                            <li>Item 13</li>
+                            <li>Item 14</li>
+                        </ul>
+                    </aside>
+                </section>
             </div>
-        </div>
-        @include ('orange.shared.footer')
+        </section>
+        <section class="footer">
+            @include ('orange.shared.footer')
+        </section>
     </div>
 @endsection
---
+++ resources/views/orange/home/partials/article.blade.php
@@ -0,0 +1,18 @@
+<article class="frontpage__article">
+    <div class="frontpage__article--thumbnail">
+        <img src="http://via.placeholder.com/240x180">
+    </div>
+    <section class="frontpage__article--content">
+        <header>
+            <h3>Post title</h3>
+        </header>
+        <article>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquam animi aut beatae dolorum eos ipsam mollitia nostrum quis repellendus. Dolorem eos eveniet possimus quaerat quo repellendus reprehenderit temporibus voluptate. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aspernatur assumenda dicta distinctio dolore dolorum eligendi enim eum, excepturi harum illo labore, maxime necessitatibus neque optio provident repellat ullam veritatis!</p>
+        </article>
+        <footer class="frontpage__article--footer">
+            Footer
+        </footer>
+    </section>
+
+
+</article>
